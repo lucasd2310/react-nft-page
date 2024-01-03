760e958262cfad9bcd0133152d1660a631d7699b
Lucas Diaz <lucas.diaz0@walmart.com>
2024-01-03 12:13:10 -0300

    added button component

--- src/components/TypeWriterText.jsx
+++ src/components/TypeWriterText.jsx
@@ -1,29 +1,42 @@
 import styled from 'styled-components'
 import Typewriter from 'typewriter-effect'
+import '@fontsource/akaya-telivigala'
+import { Button } from './Button'
 
 export function TypeWriterText() {
-  return (<Title>
-    Aprende React creando proyectos
-    <Typewriter
-      options={{
-        autoStart: true,
-        loop: true,
-      }}
-      onInit={(typewriter) => {
-        typewriter
-          .typeString(`<span class="text-1">Divertidos</span>`)
-          .pauseFor(500)
-          .deleteAll()
-          .typeString(`<span class="text-2">Increibles</span>`)
-          .pauseFor(500)
-          .deleteAll()
-          .typeString(`<span class="text-3">Y reutilizables!</span>`)
-          .pauseFor(500)
-          .deleteAll()
-          .start();
-      }}
-    />
-  </Title>)
+  return (
+    <div>
+      <Title>
+        Aprende React creando proyectos
+        <Typewriter
+          options={{
+            autoStart: true,
+            loop: true,
+          }}
+          onInit={(typewriter) => {
+            typewriter
+              .typeString(`<span class="text-1">Divertidos!</span>`)
+              .pauseFor(500)
+              .deleteAll()
+              .typeString(`<span class="text-2">Increibles!</span>`)
+              .pauseFor(500)
+              .deleteAll()
+              .typeString(`<span class="text-3">Reutilizables!</span>`)
+              .pauseFor(500)
+              .deleteAll()
+              .start()
+          }}
+        />
+      </Title>
+      <SubTitle>React Es Cool 😎💙</SubTitle>
+      <ButtonContainer>
+        <Button
+          text='Explorar curso'
+          link='https://www.udemy.com/course/react-intensivo-desde-cero-y-actualizado-para-principiantes/?couponCode=E46E575E9C9E84FE7683'
+        />
+      </ButtonContainer>
+    </div>
+  )
 }
 
 const Title = styled.h2`
@@ -55,5 +68,32 @@ const Title = styled.h2`
   }
   @media (max-width: 40em) {
     width: 90%;
+  }
+`
+const SubTitle = styled.h3`
+  font-size: ${(props) => props.theme.fontlg};
+  text-transform: capitalize;
+  color: ${(props) => `rgba(${props.theme.textRgba}, 0.6)`};
+  font-weight: 600;
+  margin-bottom: 1rem;
+  width: 80%;
+  align-self: flex-start;
+  @media (max-width: 40em) {
+    font-size: ${(props) => props.theme.fontmd};
+  }
+  @media (max-width: 48em) {
+    align-self: center;
+    text-align: center;
+  }
+`
+const ButtonContainer = styled.div`
+  width: 80%;
+  align-self: flex-start;
+  @media (max-width: 48em) {
+    align-self: center;
+    text-align: center;
+    button {
+      margin: 0 auto;
+    }
   }
 `
