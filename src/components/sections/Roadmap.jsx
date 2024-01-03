24d13a4b5ca57c1c0e57fe69bb24549b4226228e
Lucas Diaz <lucas.diaz0@walmart.com>
2024-01-03 19:07:45 -0300

    updated roadmap component

--- src/components/sections/Roadmap.jsx
+++ src/components/sections/Roadmap.jsx
@@ -9,8 +9,28 @@ export function Roadmap() {
         <SvgContainer>
           <DrawSvg />
         </SvgContainer>
+        <Items>
+          <RoadMapItem/>
+          <RoadMapItem/>
+          <RoadMapItem/>
+          <RoadMapItem/>
+          <RoadMapItem/>
+        </Items>
       </Container>
     </Section>
+  )
+}
+
+const RoadMapItem = ({ subtitle, subtext }) => {
+  return (
+    <ItemContainer>
+      <Item>
+        <Box>
+          <SubTitle>Subtitle</SubTitle>
+          <Text>subtext</Text>
+        </Box>
+      </Item>
+    </ItemContainer>
   )
 }
 
@@ -58,4 +78,100 @@ const SvgContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+`
+const Items = styled.ul`
+  list-style: none;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  @media (max-width: 48em) {
+    width: 90%;
+  }
+  & > *:nth-of-type(2n + 1) {
+    justify-content: start;
+    @media (max-width: 48em) {
+      justify-content: center;
+    }
+
+    div {
+      border-radius: 50px 0 50px 0;
+      text-align: right;
+
+      @media (max-width: 48em) {
+        border-radius: 0 50px 0 50px;
+        text-align: left;
+        p {
+          border-radius: 0 40px 0 40px;
+        }
+      }
+    }
+    p {
+      border-radius: 40px 0 40px 0;
+    }
+  }
+  & > *:nth-of-type(2n) {
+    justify-content: end;
+    @media (max-width: 48em) {
+      justify-content: center;
+    }
+    div {
+      border-radius: 0 50px 0 50px;
+      text-align: left;
+    }
+    p {
+      border-radius: 0 40px 0 40px;
+    }
+  }
+`
+const ItemContainer = styled.li`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  @media (max-width: 48em) {
+    justify-content: flex-end !important;
+  }
+`
+const Item = styled.div`
+  width: 40%;
+  height: fit-content;
+  padding: 1rem;
+  border: 3px solid ${(props) => props.theme.text};
+
+  @media (max-width: 48em) {
+    width: 70%;
+  }
+`
+const Box = styled.p`
+  height: fit-content;
+  background-color: ${(props) => props.theme.carouselColor};
+  color: ${(props) => props.theme.text};
+  padding: 1rem;
+  position: relative;
+  border: 1px solid ${(props) => props.theme.text};
+`
+const SubTitle = styled.span`
+  display: block;
+  font-size: ${(props) => props.theme.fontxl};
+  text-transform: capitalize;
+  color: ${(props) => props.theme.text};
+
+  @media (max-width: 40em) {
+    font-size: ${(props) => props.theme.fontlg};
+    font-weight: 600;
+  }
+`
+const Text = styled.span`
+  display: block;
+  font-size: ${(props) => props.theme.fontsm};
+  text-transform: capitalize;
+  color: ${(props) => props.theme.text};
+
+  font-weight: 400;
+  margin: 0.5rem 0;
+  @media (max-width: 40em) {
+    font-size: ${(props) => props.theme.fontxs};
+  }
 `
